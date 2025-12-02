cc29f883d84c9fbe7985e24c491d3b1fe588c606
Md Shabbir Ali <mshabbirali48@gmail.com>
2025-12-02 19:27:12 +0530

    data ingestion done

--- src/textSummarizer/components/data_ingestion.py
+++ src/textSummarizer/components/data_ingestion.py
@@ -12,21 +12,29 @@ class DataIngestion:
 
 
     
-    def download_file(self):
-        if not os.path.exists(self.config.local_data_file):
-            filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
-            )
-            logger.info(f"{filename} download! with following info: \n{headers}")
-        else:
-            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
+    def download_file(self)-> Path:
+        '''
+        Fetch data from the url
+        '''
 
-        
-    
+        try: 
+            dataset_url = self.config.source_URL
+            zip_download_dir = self.config.local_data_file
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
+            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+
+            file_id = dataset_url.split("/")[-2]
+            prefix = 'https://drive.google.com/uc?id='
+            gdown.download(prefix+file_id,zip_download_dir)
+
+            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+
+        except Exception as e:
+            raise e
+
     def extract_zip_file(self):
         """
-        zip_file_path: str
+        zip_file_path: Path
         Extracts the zip file into the data directory
         Function returns None
         """
